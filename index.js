4896f15cfe26ebddf3f71f43b86f57205f5b4e8f
AtulSingh205 <atulsingh07062004@gmail.com>
2025-08-14 00:42:40 +0530

    clouser

---
+++ index.js
@@ -0,0 +1,15 @@
+function abcd(){
+    let count = 0;
+    return function(){
+         count++
+         return count
+    }
+}
+let a = abcd()
+console.log(a())
+console.log(a())
+console.log(a())
+console.log(a())
+
+let b = abcd()
+console.log(b())
